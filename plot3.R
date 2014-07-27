769ced1af14d90c116b60e47f0d4c09f7ac07c42
ujwlkarn <ujwlkarn@gmail.com>
2014-07-28 01:43:38 +0530

    second

--- plot3.R
+++ plot3.R
@@ -1,12 +1,10 @@
 NEI <- readRDS("summarySCC_PM25.rds")
-NEIb <- subset(NEI, fips == "24510")
+NEIb <- subset(NEI, fips == "24510")  #created a subset for Baltimore
 
-z<-tapply(NEIb$Emissions, list(NEIb$year, NEIb$type), sum)
+#used tapply to find total emission by type and also by year
+z<-tapply(NEIb$Emissions, list(NEIb$year, NEIb$type), sum) 
 
-
+#plotted using ggplot
 png("plot3.png", width = 800, height = 600)
-
 ggplot(data=melt(z), aes(x=Var1, y=value, group=Var2, colour=Var2, shape=Var2))+geom_line()+geom_point()
-
-#ggsave(file="plot3.pmg", plot=image, width=10, height=8)
 dev.off()
