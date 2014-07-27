7ef21f04c717628620f0d6a1f88219732dcfb558
ujwlkarn <ujwlkarn@gmail.com>
2014-07-28 01:43:20 +0530

    second

--- plot2.R
+++ plot2.R
@@ -1,8 +1,10 @@
 NEI <- readRDS("summarySCC_PM25.rds")
-NEIb <- subset(NEI, fips == "24510")
+NEIb <- subset(NEI, fips == "24510") #created a subset for Baltimore 
 
-s<-tapply(NEIb$Emissions, NEIb$year, sum)
-k<-data.frame(key=names(s), value=s)
+s<-tapply(NEIb$Emissions, NEIb$year, sum) #used tapply to find total emissions per year
+k<-data.frame(key=names(s), value=s) #put the values in a dataframe k
+
+#plotted
 png("plot2.png", width = 800, height = 600)
 with(k,plot(key,value))
 dev.off()
